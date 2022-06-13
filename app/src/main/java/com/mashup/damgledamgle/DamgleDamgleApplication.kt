ce3c707ff516a0ec0f6da1a4b1f1f51faa86f540
Nilto Dahyun Kang <dahyun1238@gmail.com>
2022-06-14 04:22:42 +0900

    Add DamgleDamgleApplication

---
+++ app/src/main/java/com/mashup/damgledamgle/DamgleDamgleApplication.kt
@@ -0,0 +1,7 @@
+package com.mashup.damgledamgle
+
+import android.app.Application
+
+class DamgleDamgleApplication : Application() {
+
+}
--- app/src/main/java/com/mashup/damgledamgle/MainActivity.kt
+++ app/src/main/java/com/mashup/damgledamgle/MainActivity.kt
@@ -19,22 +19,9 @@ class MainActivity : ComponentActivity() {
             DamgleDamgleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Text("DamgleDamgle")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DamgleDamgleTheme {
-        Greeting("Android")
     }
 }
