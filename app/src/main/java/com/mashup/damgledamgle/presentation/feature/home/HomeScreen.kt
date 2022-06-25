937a8c8f8c37073265a321d5832db5ed76b1d46a
ddyeon <cab05102@naver.com>
2022-06-25 15:50:44 +0900

    Add naver maps library

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/HomeScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/HomeScreen.kt
@@ -1,4 +1,15 @@
 package com.mashup.damgledamgle.presentation.feature.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import com.naver.maps.map.MapView
+
+@Composable
 fun HomeScreen() {
 }
+
+
