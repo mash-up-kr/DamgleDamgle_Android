d0fe8dd5c60f35750647cc64ed6f387d0136c1fd
정민지 <s2018s38@e-mirim.hs.kr>
2022-07-03 23:28:12 +0900

    Add SplashScreen image and animation
    - 스플래시 화면에 들어가는 담글담글 로고 이미지 추가 및 레이아웃 구성
    - 3초 딜레이 후 홈화면으로 이동
    - 임시로 스플래시 애니메이션 추가

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/splash/SplashScreen.kt
@@ -1,25 +1,57 @@
 package com.mashup.damgledamgle.presentation.feature.splash
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.mashup.damgledamgle.R
+import com.mashup.damgledamgle.ui.theme.Grey500
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Scaffold(
-            backgroundColor = MaterialTheme.colors.primary,
-            contentColor = MaterialTheme.colors.secondary,
-            content =
-            {
-                Text(text = "Splash")
-            }
-    )
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
     LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
         delay(3000L)
         navController.navigate("home_screen")
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Grey500)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.damgle_splash),
+            contentDescription = "DamgleDamgle Splash Image",
+            modifier = Modifier
+                .width(210.dp)
+                .scale(scale.value)
+        )
     }
 }
