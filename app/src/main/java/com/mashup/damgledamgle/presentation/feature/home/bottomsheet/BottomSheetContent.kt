47d5fa67583ace046ce144a9036595218fa1c1fb
Nilto Dahyun Kang <dahyun1238@gmail.com>
2022-07-04 00:49:54 +0900

    Add BottomSheet Limit PutStoryBox

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/bottomsheet/BottomSheetContent.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/bottomsheet/BottomSheetContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/bottomsheet/PutStoryBox.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/bottomsheet/PutStoryBox.kt
@@ -1,6 +1,7 @@
 package com.mashup.damgledamgle.presentation.feature.home.bottomsheet
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -10,6 +11,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.mashup.damgledamgle.R
+import com.mashup.damgledamgle.presentation.component.ErrorText
 
 @Composable
 fun PutStoryBox(
@@ -25,21 +27,53 @@ fun PutStoryBox(
             backgroundColor = backGroundColor
     ) {
         var text by rememberSaveable { mutableStateOf("") }
-        TextField(
-                modifier = Modifier
-                        .fillMaxSize(),
-                value = text,
-                onValueChange = {
-                    text = it
-                },
-                colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = backGroundColor,
-                        textColor = textColor,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        disabledIndicatorColor = Color.Transparent
-                ),
-                label = { Text(text = stringResource(id = R.string.home_bottomsheet_leave_your_own_story)) }
-        )
+        var textErrorState by rememberSaveable { mutableStateOf(false) }
+        Column(verticalArrangement = SpaceBetween) {
+            val maxTextFieldChar = 100
+            val maxLine = 6
+            Column {
+                TextField(
+                        modifier = Modifier
+                                .wrapContentHeight(),
+                        value = text,
+                        onValueChange = {
+                            when {
+                                it.lines().count() > maxLine -> {
+                                    return@TextField
+                                }
+                                it.length <= maxTextFieldChar -> {
+                                    textErrorState = false
+                                    text = it
+                                }
+                                else -> {
+                                    textErrorState = true
+                                }
+                            }
+                        },
+                        colors = TextFieldDefaults.textFieldColors(
+                                backgroundColor = backGroundColor,
+                                textColor = textColor,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent
+                        ),
+                        maxLines = 6,
+                        placeholder = { Text(text = stringResource(id = R.string.home_bottomsheet_leave_your_own_story)) }
+                )
+                if (textErrorState)
+                    ErrorText(
+                            modifier = Modifier.padding(horizontal = 24.dp),
+                            text = stringResource(id = R.string.home_bottomsheet_you_can_enter_up_to_100_characters)
+                    )
+            }
+            Row(
+                    modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+            ) {
+                Text(text = "${text.count()}/100자")
+            }
+        }
     }
 }
