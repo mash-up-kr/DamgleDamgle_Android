442bc971db9523cb3b196ff17cfd7be74b62dbe9
정민지 <s2018s38@e-mirim.hs.kr>
2022-07-08 21:53:41 +0900

    Refactor
    - 코드리뷰 반영
    - BackPressHandler -> BackPressInterceptor로 네이밍 변경
    - 뒤로가기 눌렀을 때 기본으로는 1.5초 체크해서 액티비티를 종료시키지만, 바텀시트 등에서 뒤로가기 눌렀을 때는 hidden 등 옵션을 처리할 수 있도록 수정
    - SplashScreen.kt에 필요없는 주석 제거

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/OnboardingScreen.kt
@@ -1,12 +1,10 @@
 package com.mashup.damgledamgle.presentation.feature.onboarding
 
-import android.app.Activity
-import android.widget.Toast
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
-import com.mashup.damgledamgle.presentation.common.BackPressHandler
+import com.mashup.damgledamgle.presentation.common.BackPressInterceptor
 
 /**
  *  OnboardingScreen.kt
@@ -18,16 +16,8 @@ import com.mashup.damgledamgle.presentation.common.BackPressHandler
 @Composable
 fun OnboardingScreen(navController: NavHostController) {
     val context = LocalContext.current
-    var backPressWaitTime by remember { mutableStateOf(0L) }
 
-    BackPressHandler {
-        if (System.currentTimeMillis() - backPressWaitTime >= 1500) { // 1.5초 안에 뒤로가기 두 번 눌러야 앱 종료.
-            backPressWaitTime = System.currentTimeMillis()
-            Toast.makeText(context, "뒤로가기 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
-        } else {
-            (context as? Activity)?.finish()
-        }
-    }
+    BackPressInterceptor(context)
 
     Text("onboarding")
 }
