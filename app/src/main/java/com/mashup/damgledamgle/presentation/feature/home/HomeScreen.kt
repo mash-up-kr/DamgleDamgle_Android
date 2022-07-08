a5f94955fa7c0f3c73ea4af72ce3775d46acb28e
ddyeon <cab05102@naver.com>
2022-07-09 01:12:48 +0900

    feat MaintoolBar
    -tool bar 변경, 아이콘 적용
    -damgle color 적용

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/HomeScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/HomeScreen.kt
@@ -1,11 +1,17 @@
 package com.mashup.damgledamgle.presentation.feature.home
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.mashup.damgledamgle.R
 import com.mashup.damgledamgle.presentation.navigation.Screen
 import com.mashup.damgledamgle.presentation.feature.toolbar.MainToolBar
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
@@ -14,32 +20,24 @@ import com.naver.maps.map.compose.ExperimentalNaverMapApi
 @Composable
 fun HomeScreen(navController: NavHostController) {
     val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
-    Scaffold(
+    BottomSheetScaffold(
         topBar = {
             MainToolBar(
-                title = "Position"
-            )
-        }
+                title = "서울시 마포구"
+            ) { navController.navigate(Screen.Mypage.route) }
+        },
+        sheetBackgroundColor = Color.Gray,
+        sheetShape = RoundedCornerShape(
+            topStart = 24.dp,
+            topEnd = 24.dp
+        ),
+        sheetContent = {
+            BottomSheetContent(bottomSheetScaffoldState)
+        },
+        sheetPeekHeight = 100.dp,
+        scaffoldState = bottomSheetScaffoldState,
     ) {
-        BottomSheetScaffold(
-                sheetBackgroundColor = Color.Gray,
-                sheetShape = RoundedCornerShape(
-                        topStart = 24.dp,
-                        topEnd = 24.dp
-                ),
-                sheetContent = {
-                    BottomSheetContent(bottomSheetScaffoldState)
-                },
-                sheetPeekHeight = 100.dp,
-                scaffoldState = bottomSheetScaffoldState,
-        ) {
-            // TODO(minji): 임시 버튼. 추후 삭제 필요.
-            Button(onClick = { navController.navigate(Screen.Mypage.route) }) {
-                Text(text = "Mypage")
-            }
-
-            MapScreen()
-        }
+        MapScreen()
     }
 }
 
