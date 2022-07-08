d95232ed3e27ccec6f4d520b18df673216bc1d90
ddyeon <cab05102@naver.com>
2022-07-09 01:47:17 +0900

    Add Location Permission
     - 위치정보를 위한 permission추가
    (Activity에 있는 퍼미션은 임시 추후 삭제 필요)
    - gms gradle 추가

--- app/src/main/java/com/mashup/damgledamgle/MainActivity.kt
+++ app/src/main/java/com/mashup/damgledamgle/MainActivity.kt
@@ -5,25 +5,44 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.mashup.damgledamgle.presentation.feature.home.map.ProvideLocationSource
 import com.mashup.damgledamgle.presentation.navigation.DamgleDamgleNavGraph
 import com.mashup.damgledamgle.ui.theme.DamgleDamgleTheme
+import com.naver.maps.map.util.FusedLocationSource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
+    private val locationSource: FusedLocationSource by lazy {
+        FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            DamgleDamgleTheme {
-                navController = rememberNavController()
-                DamgleDamgleNavGraph(
-                    navController = navController,
-                )
+            ProvideLocationSource(locationSource = locationSource) {
+                DamgleDamgleTheme {
+                    navController = rememberNavController()
+                    DamgleDamgleNavGraph(navController = navController)
+                }
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
