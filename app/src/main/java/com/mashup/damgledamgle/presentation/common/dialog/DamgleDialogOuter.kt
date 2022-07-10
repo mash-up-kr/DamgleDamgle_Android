41d193d9f105d4f0fa8bd15130eb553b6efa2990
Nilto Dahyun Kang <dahyun1238@gmail.com>
2022-07-11 00:36:16 +0900

    Redesign Dialog

--- app/src/main/java/com/mashup/damgledamgle/presentation/common/dialog/DamgleDialogOuter.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/common/dialog/DamgleDialogOuter.kt
@@ -1,10 +1,10 @@
 package com.mashup.damgledamgle.presentation.common.dialog
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.mashup.damgledamgle.ui.theme.Gray400
@@ -12,14 +12,15 @@ import com.mashup.damgledamgle.ui.theme.Gray400
 
 @Composable
 fun DamgleDialogOuter(
-    modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+        modifier: Modifier = Modifier,
+        content: @Composable () -> Unit
 ) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(24.dp),
-        backgroundColor = Gray400
+            modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth(),
+            shape = RoundedCornerShape(24.dp),
+            backgroundColor = Gray400
     ) {
         content()
     }
