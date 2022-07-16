e415455672c9870153af3a523510a6f0a3b3f81b
Nilto Dahyun Kang <dahyun1238@gmail.com>
2022-07-16 21:06:57 +0900

    Add ReactionState Logic

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/all_damgle_list/AllDamgleListScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/all_damgle_list/AllDamgleListScreen.kt
@@ -1,13 +1,20 @@
 package com.mashup.damgledamgle.presentation.feature.all_damgle_list
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.mashup.damgledamgle.ui.theme.Grey500
+import com.mashup.damgledamgle.R
+import com.mashup.damgledamgle.ui.theme.*
 
 @Composable
 fun AllDamgleListScreen(navController: NavHostController, viewModel: AllDamgleListViewModel = hiltViewModel()) {
@@ -15,15 +22,56 @@ fun AllDamgleListScreen(navController: NavHostController, viewModel: AllDamgleLi
         Box(
                 modifier = Modifier
                         .fillMaxSize()
-                        .background(Grey500)
+                        .background(
+                                brush = Brush.verticalGradient(
+                                        0.15f to Grey500,
+                                        0.3f to Orange400,
+                                        0.38f to Orange600,
+                                        0.7f to Orange600,
+                                        0.76f to Orange400,
+                                        0.85f to Grey500,
+                                )
+                        )
         ) {
-            Column {
-                viewModel.reactionBoxState.forEach { boxState ->
-                    ReactionBox(
-                            boxState.value,
-                            { viewModel.reactMain(boxState.key) },
-                            { reaction -> viewModel.react(boxState.key, reaction) }
+            Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                        modifier = Modifier
+                                .height(66.dp)
+                                .fillMaxWidth(),
+                ) {
+                    Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            text = "담벼락 전체보기"
                     )
+                    Image(
+                            painter = painterResource(id = R.drawable.ic_close),
+                            contentDescription = null,
+                            modifier = Modifier
+                                    .width(24.dp)
+                                    .align(Alignment.CenterEnd)
+                                    .offset(x = (-16).dp)
+                                    .clickable { navController.popBackStack() }
+                    )
+                }
+                LazyColumn {
+                    item {
+                        Row {
+                            Text(text = "최신순")
+                            Text(text = "인기순")
+                        }
+                    }
+                    viewModel.reactionBoxState.forEach { boxState ->
+                        item {
+                            DamgleStoryBox(
+                                    boxState.value,
+                                    { viewModel.reactMain(boxState.key) },
+                                    { reaction -> viewModel.react(boxState.key, reaction) }
+                            )
+                        }
+                    }
                 }
             }
         }
