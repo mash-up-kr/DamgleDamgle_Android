f57675c7fdb794cbc21b87a66bb385a65dcb42f1
Nilto Dahyun Kang <dahyun1238@gmail.com>
2022-07-17 22:29:07 +0900

    Add Base AllDamgleStory Design

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/all_damgle_list/AllDamgleListScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/all_damgle_list/AllDamgleListScreen.kt
@@ -1,15 +1,18 @@
 package com.mashup.damgledamgle.presentation.feature.all_damgle_list
 
 import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -20,55 +23,69 @@ import com.mashup.damgledamgle.ui.theme.*
 fun AllDamgleListScreen(navController: NavHostController, viewModel: AllDamgleListViewModel = hiltViewModel()) {
     Scaffold {
         Box(
-                modifier = Modifier
-                        .fillMaxSize()
-                        .background(
-                                brush = Brush.verticalGradient(
-                                        0.15f to Grey500,
-                                        0.3f to Orange400,
-                                        0.38f to Orange600,
-                                        0.7f to Orange600,
-                                        0.76f to Orange400,
-                                        0.85f to Grey500,
-                                )
-                        )
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        0.15f to Grey500,
+                        0.3f to Orange400,
+                        0.38f to Orange600,
+                        0.7f to Orange600,
+                        0.76f to Orange400,
+                        0.85f to Grey500,
+                    )
+                )
         ) {
             Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Box(
-                        modifier = Modifier
-                                .height(66.dp)
-                                .fillMaxWidth(),
+                    modifier = Modifier
+                        .height(66.dp)
+                        .fillMaxWidth(),
                 ) {
                     Text(
-                            modifier = Modifier.align(Alignment.Center),
-                            text = "담벼락 전체보기"
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "담벼락 전체보기"
                     )
                     Image(
-                            painter = painterResource(id = R.drawable.ic_close),
-                            contentDescription = null,
-                            modifier = Modifier
-                                    .width(24.dp)
-                                    .align(Alignment.CenterEnd)
-                                    .offset(x = (-16).dp)
-                                    .clickable { navController.popBackStack() }
+                        painter = painterResource(id = R.drawable.ic_close),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .width(24.dp)
+                            .align(Alignment.CenterEnd)
+                            .offset(x = (-16).dp)
+                            .clickable { navController.popBackStack() }
                     )
                 }
                 LazyColumn {
                     item {
-                        Row {
-                            Text(text = "최신순")
-                            Text(text = "인기순")
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Row(modifier = Modifier.padding(horizontal = 20.dp)) {
+                            DamgleStorySort.values().forEach {
+                                val interactionSource = remember { MutableInteractionSource() }
+                                Text(
+                                    modifier = Modifier
+                                        .padding(end = 8.dp)
+                                        .clickable(
+                                            interactionSource = interactionSource,
+                                            indication = null
+                                        ) { viewModel.changeDamgleSortStrategy(it) },
+                                    text = it.key,
+                                    color = if (viewModel.damgleSortStrategy == it) Black else Gray800,
+                                    fontWeight = if (viewModel.damgleSortStrategy == it) FontWeight.Bold else FontWeight.Normal,
+                                )
+                            }
                         }
+                        Spacer(modifier = Modifier.height(24.dp))
                     }
                     viewModel.reactionBoxState.forEach { boxState ->
                         item {
                             DamgleStoryBox(
-                                    boxState.value,
-                                    { viewModel.reactMain(boxState.key) },
-                                    { reaction -> viewModel.react(boxState.key, reaction) }
+                                boxState.value,
+                                { viewModel.reactMain(boxState.key) },
+                                { reaction -> viewModel.react(boxState.key, reaction) }
                             )
                         }
                     }
--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/all_damgle_list/DamgleStoryBox.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/all_damgle_list/DamgleStoryBox.kt
@@ -1,13 +1,11 @@
 package com.mashup.damgledamgle.presentation.feature.all_damgle_list
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -16,41 +14,68 @@ import com.mashup.damgledamgle.ui.theme.*
 
 @Composable
 fun DamgleStoryBox(
-        reactionBoxState: ReactionBoxState,
-        onClickNowReaction: () -> Unit,
-        onClickReaction: (Reaction) -> Unit,
+    reactionBoxState: ReactionBoxState,
+    onClickNowReaction: () -> Unit,
+    onClickReaction: (Reaction) -> Unit,
 ) {
     Column(
-            modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
     ) {
         Text(
-                text = "MAPOGU\n" +
-                        "YANGHWARO",
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold,
-                color = Gray1000
+            text = "MAPOGU\n" +
+                    "YANGHWARO",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Gray1000
         )
         Spacer(modifier = Modifier.height(7.dp))
-        Row {
-
-        }
-        Spacer(modifier = Modifier.height(32.dp))
-        Card(
+        Row(modifier = Modifier.height(16.dp)) {
+            Text(
+                text = "말많은 11번째 코알라",
+                fontSize = 13.sp
+            )
+            Text(
+                text = " · ME",
+                fontWeight = FontWeight.Bold,
+                fontSize = 13.sp
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Divider(
+                color = Gray800,
+                thickness = 1.dp,
                 modifier = Modifier
-                        .height(320.dp)
-                        .fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp),
-                backgroundColor = Gray400
+                    .align(Alignment.CenterVertically)
+                    .weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "5분전",
+                color = Yellow,
+                fontSize = 13.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Card(
+            modifier = Modifier
+                .height(320.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            backgroundColor = Gray400
         ) {
-
+            Text(
+                text = "안녕하세요! 오늘은 이 카페에서 공감해조팀에서 회의를 하였어요~~ 다들 여기서 팀플해도 좋을듯용~~! 주인이 카공해도 뭐라고 안함~~참고하세요~~~!! 지금 넘 졸린데 오늘 일찍 자고 싶어서 커피를 마시지 않고 버틸 거에요~!!",
+                fontSize = 16.sp,
+                maxLines = 6,
+                modifier = Modifier.padding(end = 24.dp, start = 24.dp, top = 24.dp),
+            )
         }
         Spacer(modifier = Modifier.height(16.dp))
         ReactionBox(
-                reactionBoxState,
-                { onClickNowReaction() },
-                { reaction -> onClickReaction(reaction) }
+            reactionBoxState,
+            { onClickNowReaction() },
+            { reaction -> onClickReaction(reaction) }
         )
         Spacer(modifier = Modifier.height(32.dp))
     }
