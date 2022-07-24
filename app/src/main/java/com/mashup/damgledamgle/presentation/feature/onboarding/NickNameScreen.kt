80de9256f760e846e03a4b50c6164961bf9846d3
정민지 <s2018s38@e-mirim.hs.kr>
2022-07-25 00:34:12 +0900

    Add NickNameScreen, NickNameBox(Composable) UI & Add OnboardingViewModel
    - 닉네임 스크린 및 닉네임 컴포저블 함수 추가 UI 추가
    - OnboardingViewModel 추가
    - ic_refresh 추가

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/NickNameScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/NickNameScreen.kt
@@ -4,13 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.mashup.damgledamgle.R
 import com.mashup.damgledamgle.ui.theme.*
 
@@ -18,7 +23,7 @@ import com.mashup.damgledamgle.ui.theme.*
  *  NickNameScreen.kt
  *
  *  Created by Minji Jeong on 2022/07/06
- *  Copyright © 2022 GwanakMT All rights reserved.
+ *  Copyright © 2022 MashUp All rights reserved.
  */
 
 @Composable
@@ -26,6 +31,10 @@ fun NickNameScreen(
     finishMakeNickName: (() -> Unit)? = null
 ) {
     val context = LocalContext.current
+    val viewModel: OnboardingViewModel = hiltViewModel()
+
+    // init data
+    viewModel.getNickName()
 
     Column(
         modifier = Modifier
@@ -47,6 +56,32 @@ fun NickNameScreen(
                 color = Gray900,
                 modifier = Modifier.padding(start = 20.dp)
             )
+
+            Text(
+                text = context.getString(
+                    R.string.nickname_placeholder_count,
+                    viewModel.nickName.collectAsState().value.nth
+                ),
+                style = pretendardTextStyle.bodyMedium24,
+                color = Orange500,
+                modifier = Modifier.padding(top = 56.dp, start = 24.dp)
+            )
+
+            Row(
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 16.dp)
+            ) {
+                NickNameBox(viewModel.nickName.collectAsState().value.adjective) {
+                    viewModel.refreshNickName(viewModel.nickName.value.copy(
+                        adjective = ""
+                    ))
+                }
+
+                NickNameBox(viewModel.nickName.collectAsState().value.noun) {
+                    viewModel.refreshNickName(viewModel.nickName.value.copy(
+                        noun = ""
+                    ))
+                }
+            }
         }
 
         Column(modifier = Modifier.weight(1f, false)) {
@@ -72,4 +107,48 @@ fun NickNameScreen(
             )
         }
     }
+}
+
+@Composable
+fun NickNameBox(
+    nickName: String,
+    onClickRefresh: (() -> Unit)? = null
+) {
+    Box(
+        Modifier
+            .width(IntrinsicSize.Max)
+            .padding(end = 16.dp)
+    ) {
+        Text(
+            text = nickName,
+            style = pretendardTextStyle.bodyMedium24,
+            color = Gray1000,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(start = 4.dp, end = 32.dp, bottom = 8.dp)
+                .wrapContentSize()
+        )
+
+        Divider(
+            color = Gray1000,
+            modifier = Modifier.align(Alignment.BottomStart),
+            thickness = 2.dp
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_refresh),
+            contentDescription = "",
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 4.dp)
+                .size(24.dp)
+                .clickable { onClickRefresh?.invoke() }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNickNameBox() {
+    NickNameBox("닉네임")
 }
---
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/model/NickName.kt
@@ -0,0 +1,15 @@
+package com.mashup.damgledamgle.presentation.feature.onboarding.model
+
+/**
+ *  NickName.kt
+ *
+ *  Created by Minji Jeong on 2022/07/24
+ *  Copyright © 2022 MashUp All rights reserved.
+ */
+
+data class NickName(
+    val fullName: String = "",
+    val adjective: String = "",
+    val noun: String = "",
+    val nth: Int = 0,
+)
