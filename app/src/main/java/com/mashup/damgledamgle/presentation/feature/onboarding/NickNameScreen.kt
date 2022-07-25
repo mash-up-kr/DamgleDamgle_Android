4e821d8baebee57a039cb671ca15af9f6fcdbb2c
정민지 <s2018s38@e-mirim.hs.kr>
2022-07-26 00:57:25 +0900

    Add ViewState sealed class
    - ViewState 추가 (Success, Error, Loading)

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/NickNameScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/NickNameScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mashup.damgledamgle.R
+import com.mashup.damgledamgle.presentation.common.ViewState
 import com.mashup.damgledamgle.ui.theme.*
 
 /**
@@ -33,75 +35,85 @@ fun NickNameScreen(
     val context = LocalContext.current
     val viewModel: OnboardingViewModel = hiltViewModel()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Grey500),
-        verticalArrangement = Arrangement.SpaceBetween,
-    ) {
-        Column {
-            Text(
-                text = context.getString(R.string.nickname_maintext),
-                style = pretendardTextStyle.title1Bold32,
-                color = Gray1000,
-                modifier = Modifier.padding(top = 94.dp, bottom = 8.dp, start = 20.dp),
-            )
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Grey500),
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column {
+                Text(
+                    text = context.getString(R.string.nickname_maintext),
+                    style = pretendardTextStyle.title1Bold32,
+                    color = Gray1000,
+                    modifier = Modifier.padding(top = 94.dp, bottom = 8.dp, start = 20.dp),
+                )
 
-            Text(
-                text = context.getString(R.string.nickname_subtext),
-                style = pretendardTextStyle.bodyMedium13,
-                color = Gray900,
-                modifier = Modifier.padding(start = 20.dp)
-            )
+                Text(
+                    text = context.getString(R.string.nickname_subtext),
+                    style = pretendardTextStyle.bodyMedium13,
+                    color = Gray900,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
 
-            Text(
-                text = context.getString(
-                    R.string.nickname_placeholder_count,
-                    viewModel.nickName.collectAsState().value.nth
-                ),
-                style = pretendardTextStyle.bodyMedium24,
-                color = Orange500,
-                modifier = Modifier.padding(top = 56.dp, start = 24.dp)
-            )
+                Text(
+                    text = context.getString(
+                        R.string.nickname_placeholder_count,
+                        viewModel.nickName.value.nth
+                    ),
+                    style = pretendardTextStyle.bodyMedium24,
+                    color = Orange500,
+                    modifier = Modifier.padding(top = 56.dp, start = 24.dp)
+                )
 
-            Row(
-                modifier = Modifier.padding(horizontal = 20.dp, vertical = 16.dp)
-            ) {
-                NickNameBox(viewModel.nickName.collectAsState().value.adjective) {
-                    viewModel.refreshNickName(viewModel.nickName.value.copy(
-                        adjective = ""
-                    ))
+                Row(
+                    modifier = Modifier.padding(horizontal = 20.dp, vertical = 16.dp)
+                ) {
+                    NickNameBox(viewModel.nickName.value.adjective) {
+                        viewModel.refreshNickName(
+                            viewModel.nickName.value.copy(
+                                adjective = ""
+                            )
+                        )
+                    }
+
+                    NickNameBox(viewModel.nickName.value.noun) {
+                        viewModel.refreshNickName(
+                            viewModel.nickName.value.copy(
+                                noun = ""
+                            )
+                        )
+                    }
                 }
+            }
 
-                NickNameBox(viewModel.nickName.collectAsState().value.noun) {
-                    viewModel.refreshNickName(viewModel.nickName.value.copy(
-                        noun = ""
-                    ))
-                }
+            Column(modifier = Modifier.weight(1f, false)) {
+                Image(
+                    painter = painterResource(id = R.drawable.damgle_nickname_background),
+                    contentDescription = "Damgle NickName Background",
+                    modifier = Modifier
+                        .padding(start = 38.dp, bottom = 34.dp)
+                        .height(104.dp)
+                )
+
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = context.getString(R.string.nickname_button_start),
+                    modifier = Modifier
+                        .background(Orange500)
+                        .fillMaxWidth()
+                        .height(64.dp)
+                        .clickable { finishMakeNickName?.invoke() }
+                        .padding(vertical = 20.dp),
+                    style = pretendardTextStyle.bodyMedium18,
+                    color = White
+                )
             }
         }
 
-        Column(modifier = Modifier.weight(1f, false)) {
-            Image(
-                painter = painterResource(id = R.drawable.damgle_nickname_background),
-                contentDescription = "Damgle NickName Background",
-                modifier = Modifier
-                    .padding(start = 38.dp, bottom = 34.dp)
-                    .height(104.dp)
-            )
-
-            Text(
-                textAlign = TextAlign.Center,
-                text = context.getString(R.string.nickname_button_start),
-                modifier = Modifier
-                    .background(Orange500)
-                    .fillMaxWidth()
-                    .height(64.dp)
-                    .clickable { finishMakeNickName?.invoke() }
-                    .padding(vertical = 20.dp),
-                style = pretendardTextStyle.bodyMedium18,
-                color = White
-            )
+        if (viewModel.uiState.collectAsState().value is ViewState.Loading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center), color = Orange500)
         }
     }
 }
--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/OnboardingViewModel.kt
@@ -1,15 +1,18 @@
 package com.mashup.damgledamgle.presentation.feature.onboarding
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mashup.damgledamgle.domain.entity.NickName
 import com.mashup.damgledamgle.domain.entity.base.NetworkResponse
 import com.mashup.damgledamgle.domain.usecase.onboarding.GetRandomNickNameUseCase
+import com.mashup.damgledamgle.presentation.common.ViewState
 import com.mashup.damgledamgle.presentation.feature.onboarding.model.NickNameModel
 import com.mashup.damgledamgle.presentation.feature.onboarding.model.mapper.NickNameMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,8 +28,10 @@ class OnboardingViewModel @Inject constructor(
     private val getRandomNickNameUseCase: GetRandomNickNameUseCase,
     private val nickNameMapper: NickNameMapper,
 ) : ViewModel() {
-    private val _nickName = MutableStateFlow(NickNameModel())
-    val nickName: StateFlow<NickNameModel> = _nickName
+    private val _uiState = MutableStateFlow<ViewState<*>>(ViewState.Loading)
+    val uiState: StateFlow<ViewState<*>> = _uiState.asStateFlow()
+
+    val nickName = mutableStateOf(NickNameModel())
 
     init {
         getNickName()
@@ -34,12 +39,15 @@ class OnboardingViewModel @Inject constructor(
 
     private fun getNickName() {
         viewModelScope.launch {
+            _uiState.emit(ViewState.Loading)
             setNickName(getRandomNickNameUseCase())
         }
     }
 
     fun refreshNickName(nickName: NickNameModel) {
         viewModelScope.launch {
+            _uiState.emit(ViewState.Loading)
+
             if (nickName.adjective.isEmpty()) {
                 setNickName(getRandomNickNameUseCase(noun = nickName.noun))
             } else if (nickName.noun.isEmpty()) {
@@ -51,10 +59,11 @@ class OnboardingViewModel @Inject constructor(
     private fun setNickName(result: NetworkResponse<NickName>) {
         when(result) {
             is NetworkResponse.Success -> {
-                _nickName.value = nickNameMapper.mapToModel(result.data)
+                nickName.value = nickNameMapper.mapToModel(result.data)
+                _uiState.value = ViewState.Success(null)
             }
             is NetworkResponse.Error -> {
-
+                _uiState.value = ViewState.Error(result.exception.message.toString())
             }
         }
     }
