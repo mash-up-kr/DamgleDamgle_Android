3da49804291e83a64fae6502db7f7f4236f891e7
정민지 <s2018s38@e-mirim.hs.kr>
2022-07-25 23:55:33 +0900

    Add Network(Retrofit + Moshi) Module & Add GetRandomNickName logic
    - 네트워크 모듈 생성(Retrofit + Moshi)
    - 랜덤 닉네임 불러오는 로직 추가 (실제 네트워크 통신)

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/onboarding/OnboardingViewModel.kt
@@ -2,7 +2,10 @@ package com.mashup.damgledamgle.presentation.feature.onboarding
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.mashup.damgledamgle.presentation.feature.onboarding.model.NickName
+import com.mashup.damgledamgle.domain.entity.base.NetworkResponse
+import com.mashup.damgledamgle.domain.usecase.onboarding.GetRandomNickNameUseCase
+import com.mashup.damgledamgle.presentation.feature.onboarding.model.NickNameModel
+import com.mashup.damgledamgle.presentation.feature.onboarding.model.mapper.NickNameMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,23 +21,30 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OnboardingViewModel @Inject constructor(
-
+    private val getRandomNickNameUseCase: GetRandomNickNameUseCase,
+    private val nickNameMapper: NickNameMapper,
 ) : ViewModel() {
-    private val _nickName = MutableStateFlow(NickName())
-    val nickName: StateFlow<NickName> = _nickName
+    private val _nickName = MutableStateFlow(NickNameModel())
+    val nickName: StateFlow<NickNameModel> = _nickName
+
+    init {
+        getNickName()
+    }
 
     fun getNickName() {
         viewModelScope.launch {
-            _nickName.value = NickName(
-                fullName = "잠자는 첫번째 햄스터",
-                adjective = "잠자는",
-                noun = "햄스터",
-                nth = 1
-            )
+            when(val result = getRandomNickNameUseCase()) {
+                is NetworkResponse.Success -> {
+                    _nickName.value = nickNameMapper.mapToModel(result.data)
+                }
+                is NetworkResponse.Error -> {
+
+                }
+            }
         }
     }
 
-    fun refreshNickName(nickName: NickName) {
+    fun refreshNickName(nickName: NickNameModel) {
         viewModelScope.launch {
             if (nickName.adjective.isEmpty()) {
                 // TODO: 형용사 다시 조회
