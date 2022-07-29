256c3fe243187683dba93e5c4ece0f24d50e690a
Nilto Dahyun Kang <dahyun1238@gmail.com>
2022-07-30 00:08:25 +0900

    Add AllDamgleList to NavGraph

--- app/src/main/java/com/mashup/damgledamgle/presentation/navigation/DamgleDamgleNavGraph.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/navigation/DamgleDamgleNavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.mashup.damgledamgle.presentation.feature.all_damgle_list.AllDamgleListScreen
 import com.mashup.damgledamgle.presentation.feature.home.HomeScreen
 import com.mashup.damgledamgle.presentation.feature.mypage.MypageScreen
 import com.mashup.damgledamgle.presentation.feature.onboarding.OnboardingScreen
@@ -34,6 +35,10 @@ fun DamgleDamgleNavGraph(navController: NavHostController) {
         }
         composable(route = Screen.Mypage.route) {
             MypageScreen(navController = navController)
+        }
+        composable(route = Screen.AllDamgleList.route) {
+            systemUiController.setSystemBarsColor(color = Grey500)
+            AllDamgleListScreen(navController = navController)
         }
     }
 }
