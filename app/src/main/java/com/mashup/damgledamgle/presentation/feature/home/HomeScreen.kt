e7813dced22d15b0498c7a0cdab186620db55720
정민지 <s2018s38@e-mirim.hs.kr>
2022-07-31 18:32:31 +0900

    Add BackPressInterceptor to HomeScreen
    - 홈화면에 뒤로가기 인터셉터 추가
    - Splash 화면에서 다른 화면으로 이동할 때 네비게이션 stack clear

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/HomeScreen.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/HomeScreen.kt
@@ -1,11 +1,16 @@
 package com.mashup.damgledamgle.presentation.feature.home
 
+import android.app.Activity
+import android.widget.Toast
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.mashup.damgledamgle.R
+import com.mashup.damgledamgle.presentation.common.BackPressInterceptor
 import com.mashup.damgledamgle.presentation.feature.home.bottomsheet.BottomSheetContent
 import com.mashup.damgledamgle.presentation.feature.toolbar.MainToolBar
 import com.mashup.damgledamgle.presentation.navigation.Screen
@@ -14,8 +19,18 @@ import com.naver.maps.map.compose.ExperimentalNaverMapApi
 @OptIn(ExperimentalMaterialApi::class, ExperimentalNaverMapApi::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
-
+    val context = LocalContext.current
+    var backPressWaitTime by remember { mutableStateOf(0L) }
     val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+
+    BackPressInterceptor(context) {
+        if (System.currentTimeMillis() - backPressWaitTime >= 1500) { // 1.5초 안에 뒤로가기 두 번 눌러야 앱 종료.
+            backPressWaitTime = System.currentTimeMillis()
+            Toast.makeText(context, context.getString(R.string.common_toast_backpress), Toast.LENGTH_SHORT).show()
+        } else {
+            (context as? Activity)?.finish()
+        }
+    }
 
     Scaffold {
         BottomSheetScaffold(
