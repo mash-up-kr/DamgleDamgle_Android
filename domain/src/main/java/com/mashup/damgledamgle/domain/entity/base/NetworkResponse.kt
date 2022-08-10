c54b16cbae0162985a8f840d497c02f6ff42994d
정민지 <s2018s38@e-mirim.hs.kr>
2022-08-11 01:02:33 +0900

    Add <T>CoroutineScope.launchWithNetworkResult() function
    - NetworkResponse 값에 따라 이벤트를 핸들링하는 확장함수 생성
    - <T>CoroutineScope.launchWithNetworkResult()
    - @param networkResponse: NetworkResponse<T>
    - @param suspendOnSuccess: suspend CoroutineScope.(T) -> Unit
    - @param suspendOnError: suspend CoroutineScope.(Exception) -> Unit,

--- domain/src/main/java/com/mashup/damgledamgle/domain/entity/base/NetworkResponse.kt
+++ domain/src/main/java/com/mashup/damgledamgle/domain/entity/base/NetworkResponse.kt
@@ -1,5 +1,8 @@
 package com.mashup.damgledamgle.domain.entity.base
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
 /**
  *  NetworkResponse.kt
  *
@@ -10,4 +13,19 @@ package com.mashup.damgledamgle.domain.entity.base
 sealed class NetworkResponse<out T> {
     class Success<T>(val data: T) : NetworkResponse<T>()
     class Error(val exception: Exception) : NetworkResponse<Nothing>()
+}
+
+fun <T> CoroutineScope.launchWithNetworkResult(
+    networkResponse: NetworkResponse<T>,
+    suspendOnSuccess: suspend CoroutineScope.(T) -> Unit,
+    suspendOnError: suspend CoroutineScope.(Exception) -> Unit,
+) {
+    when(networkResponse) {
+        is NetworkResponse.Success -> launch {
+            suspendOnSuccess(networkResponse.data)
+        }
+        is NetworkResponse.Error -> launch {
+            suspendOnError(networkResponse.exception)
+        }
+    }
 }
