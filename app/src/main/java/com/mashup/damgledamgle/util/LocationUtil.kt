33f0e34ac4df3a8ba1e945d0a83f0d19cb3a3d39
ddyeon <cab05102@naver.com>
2022-08-19 21:11:36 +0900

    Add LocationUtil permission
    - Locationmanager 호출할 때마다 퍼미션 검사

--- app/src/main/java/com/mashup/damgledamgle/util/LocationUtil.kt
+++ app/src/main/java/com/mashup/damgledamgle/util/LocationUtil.kt
@@ -1,39 +1,55 @@
 package com.mashup.damgledamgle.util
 
-import android.annotation.SuppressLint
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.*
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.core.app.ActivityCompat
+import com.mashup.damgledamgle.MainActivity
 import com.naver.maps.geometry.LatLng
 
 object LocationUtil {
 
-    @SuppressLint("MissingPermission")
     fun getMyLocation(context : Context) : LatLng? {
 
         val locationManager = context.getSystemService(ComponentActivity.LOCATION_SERVICE) as LocationManager
-        val currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) ?:
-        locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-
-        var latLng : LatLng? = currentLocation?.latitude?.let { LatLng(it, currentLocation.longitude) }
-        val gpsListener =  object : LocationListener {
-            override fun onLocationChanged(location : Location) {
-                val latitude = location.latitude
-                val longitude = location.longitude
-                Log.d("Test", "GPS Location changed, Latitude: $latitude" +
-                    ", Longitude: $longitude")
-                latLng = LatLng(latitude,longitude)
-            }
-            override fun onProviderDisabled(provider: String) {}
-            override fun onProviderEnabled(provider: String) {}
+        var latLng : LatLng? = null
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(context, "위치 권한에 동의해야 앱 사용이 가능합니다.", Toast.LENGTH_SHORT).show()
+            /**
+             * 앱 종료 or 그대로 둘지 추가
+             */
         }
+        else {
+            val currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) ?:
+            locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-            1000,
-            50.0f,
-            gpsListener)
+            latLng = currentLocation?.latitude?.let { LatLng(it, currentLocation.longitude) }
+            val gpsListener =  object : LocationListener {
+                override fun onLocationChanged(location : Location) {
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    Log.d("Test", "GPS Location changed, Latitude: $latitude" +
+                            ", Longitude: $longitude")
+                    latLng = LatLng(latitude,longitude)
+                }
+                override fun onProviderDisabled(provider: String) {}
+                override fun onProviderEnabled(provider: String) {}
+            }
 
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000,
+                50.0f,
+                gpsListener)
+
+        }
         return latLng
     }
 
