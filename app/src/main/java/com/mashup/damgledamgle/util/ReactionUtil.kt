a1ec296fe3cbf95940c33aab267fdf598263512b
ddyeon <cab05102@naver.com>
2022-08-23 00:09:29 +0900

    Add reactionUtil
    - 가장 많은 리액션 계산

--- app/src/main/java/com/mashup/damgledamgle/util/ReactionUtil.kt
+++ app/src/main/java/com/mashup/damgledamgle/util/ReactionUtil.kt
@@ -1,19 +1,18 @@
 package com.mashup.damgledamgle.util
 
 import com.mashup.damgledamgle.domain.entity.Damgle
-import com.mashup.damgledamgle.presentation.feature.home.model.MarkerModel
 
 object ReactionUtil {
 
-    fun getMainIcon(groupList : List<MarkerModel>) : String {
+    fun getMainIcon(groupList : List<Damgle>) : String {
         var mainIcon : String? = null
         groupList.forEach { damgle ->
-            mainIcon = damgle.damgle.reactionSummary.maxWithOrNull(
+            mainIcon = damgle.reactionSummary.maxWithOrNull(
                 Comparator.comparing {
                     it.count
                 }
             )?.type
         }
-        return if(mainIcon == null) "best" else mainIcon as String
+        return if(mainIcon == null) "nothing" else mainIcon as String
     }
 }
