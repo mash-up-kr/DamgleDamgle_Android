5602c686bbcc604523c6135c6fbf271ad0e16d8a
ddyeon <cab05102@naver.com>
2022-08-24 01:34:49 +0900

    Add prefrence damgle repository

--- data/src/main/java/com/mashup/damgledamgle/repository/remote/MapRepositoryImpl.kt
+++ data/src/main/java/com/mashup/damgledamgle/repository/remote/MapRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.mashup.damgledamgle.repository.remote
 
+import android.content.Context
 import com.mashup.damgledamgle.data.BuildConfig
 import com.mashup.damgledamgle.domain.entity.Damgle
 import com.mashup.damgledamgle.domain.entity.GeoResult
@@ -9,13 +10,26 @@ import com.mashup.damgledamgle.mapper.DamgleMapper
 import com.mashup.damgledamgle.mapper.geocodeMapper
 import com.mashup.damgledamgle.network.DamgleApi
 import com.mashup.damgledamgle.network.NaverApi
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class MapRepositoryImpl @Inject constructor(
     private val naverApi: NaverApi,
     private val damgleApi : DamgleApi,
-    private val damgleMapper : DamgleMapper
+    private val damgleMapper : DamgleMapper,
+    @ApplicationContext private val context: Context
     ) : MapRepository {
+
+    private val prefs =
+        context.getSharedPreferences(PREFERENCE_FILE_NAME, Context.MODE_PRIVATE)
+
+    override fun getLastEntryDamgleDay(): String {
+       return prefs.getString(LAST_ENTRY_DAY, "") ?: ""
+    }
+
+    override fun setLastEntryDamgleDay(date: String) {
+        prefs.edit().putString(LAST_ENTRY_DAY, date).apply()
+    }
 
     override suspend fun getReverseGeocoding(
         coors: String): Result<GeoResult> {
@@ -51,5 +65,10 @@ class MapRepositoryImpl @Inject constructor(
         } catch (e : Exception) {
             Result.Error(e)
         }
+    }
+
+    companion object {
+        private const val PREFERENCE_FILE_NAME = "DamgleDamgle"
+        private const val LAST_ENTRY_DAY = "LAST_ENTRY_DAMGLE_DAY"
     }
 }
