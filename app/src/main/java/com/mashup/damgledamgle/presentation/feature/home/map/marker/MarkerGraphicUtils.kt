d83700bff3814ae840e3177147a68084ddd4794a
ddyeon <cab05102@naver.com>
2022-08-24 01:34:00 +0900

    Fix reading marker icon
    - 읽음 처리 아이콘 빼기

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/map/marker/MarkerGraphicUtils.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/map/marker/MarkerGraphicUtils.kt
@@ -12,7 +12,7 @@ import com.mashup.damgledamgle.presentation.feature.home.model.GroupMarkerInfo
 
 
 @SuppressLint("InflateParams")
-fun makeCustomMarkerView(markerInfo: MarkerInfo, context: Context) = (
+fun makeCustomMarkerView(markerInfo: GroupMarkerInfo, context: Context) = (
         (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
             .inflate(R.layout.item_marker,null) as ConstraintLayout).apply {
 
@@ -20,29 +20,26 @@ fun makeCustomMarkerView(markerInfo: MarkerInfo, context: Context) = (
         if(markerInfo.isMine) R.drawable.ic_text_balloon_me
         else R.drawable.ic_text_balloon
 
-//    var res : Int = 0
-//    res = when(markerInfo.) {
-//        "angry" -> R.drawable.ic_angry_small
-//        "sad" -> R.drawable.ic_sad_small
-//        "amazing" -> R.drawable.ic_amazing_small
-//        "best" -> R.drawable.ic_best_small
-//        "like" -> R.drawable.ic_heart_small
-//        else -> R.drawable.ic_best_small
-//    }
+    val res = when(markerInfo.mainIcon) {
+        "angry" -> R.drawable.ic_angry_small
+        "sad" -> R.drawable.ic_sad_small
+        "amazing" -> R.drawable.ic_amazing_small
+        "best" -> R.drawable.ic_best_small
+        "like" -> R.drawable.ic_heart_small
+        else -> R.drawable.ic_noreaction_small
+    }
 
-    (this.getViewById(R.id.marker_icon) as ImageView).setImageResource(markerInfo.resId)
+    (this.getViewById(R.id.marker_icon) as ImageView).setImageResource(res)
     (this.getViewById(R.id.balloon_background) as ImageView).setImageResource(balloonRes)
 
-    if(!markerInfo.isRead && markerInfo.size <= 1) {
+    if(markerInfo.count <= 1) {
         (this.getViewById(R.id.count_text) as TextView).visibility = View.GONE
         (this.getViewById(R.id.count_icon) as ImageView).visibility = View.GONE
-        (this.getViewById(R.id.notify_icon) as ImageView).visibility = View.VISIBLE
     }
-    else if(markerInfo.size > 1) {
-        (this.getViewById(R.id.notify_icon) as ImageView).visibility = View.GONE
+    else {
         (this.getViewById(R.id.count_icon) as ImageView).visibility = View.VISIBLE
         (this.getViewById(R.id.count_text) as TextView).visibility = View.VISIBLE
         (this.getViewById(R.id.count_text) as TextView).text =
-            if(markerInfo.size > 99) "99+" else markerInfo.size.toString()
+            if(markerInfo.count > 99) "99+" else markerInfo.count.toString()
     }
 }
