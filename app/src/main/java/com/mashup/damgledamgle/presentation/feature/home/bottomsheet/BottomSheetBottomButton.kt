1284cdcd32bd858bf314896c2f6e2cd50368f555
Nilto Dahyun Kang <dahyun1238@gmail.com>
2022-09-02 02:15:17 +0900

    Fix BottomSheetBottomButton

--- app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/bottomsheet/BottomSheetBottomButton.kt
+++ app/src/main/java/com/mashup/damgledamgle/presentation/feature/home/bottomsheet/BottomSheetBottomButton.kt
@@ -1,29 +1,30 @@
 package com.mashup.damgledamgle.presentation.feature.home.bottomsheet
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.mashup.damgledamgle.R
 import com.mashup.damgledamgle.ui.theme.*
 
 @Composable
 fun BottomSheetBottomButton(text: String, onClickBottomSheetButton: () -> Unit) {
-    // TODO 글자수에 따라 Clickable, 디자인 변경
-    Button(
-        onClick = {
-            if (text.isNotEmpty()) onClickBottomSheetButton()
-        },
-        colors = ButtonDefaults.buttonColors(backgroundColor = if (text.isNotEmpty()) Black else Gray600),
+    Box(
         modifier = Modifier
             .height(64.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable { if (text.isNotEmpty()) onClickBottomSheetButton() }
+            .background(
+                color = if (text.isNotEmpty()) Black else Gray600
+            ),
     ) {
         Text(
+            modifier = Modifier.align(Alignment.Center),
             text = stringResource(id = R.string.home_bottomsheet_leave_a_story),
             style = pretendardTextStyle.bodyMedium18,
             color = if (text.isNotEmpty()) White else Gray400
